82315b9ee3579dad6d24aaf7676ab60646b0febb
EugeneMaikov <eugene-maikov2000@yandex.ru>
2021-06-09 22:37:50 +0300

    commit__

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,80 @@
 $(function () {
-    // Menu nav toggle
+    //============= Меню ==============
     $("#nav_toggle").on("click", function (event) {
-            event.preventDefault();
-            $(this).toggleClass("active");
-            $(".aside").toggleClass("aside-active");
-            $(".modal").toggleClass("show");
-            $(".menu").toggleClass("menu-transform");
-        }),
+        event.preventDefault();
+        $(this).toggleClass("active");
+        $(".aside").toggleClass("aside-active");
+        $(".modal").toggleClass("show");
+        $(".menu").toggleClass("menu-transform");
+    }),
+
+    // Выбор доменного имени
+    $(".webadress__btn-change").on("click", function(){
+        $(".webadress").addClass("webadress-notActive");
+        $(".domen").addClass("domen-active");
+    }),
+    $(".domen__btn-cancel").on("click", function(){
+        $(".webadress").removeClass("webadress-notActive");
+        $(".domen").removeClass("domen-active");
+    }),
+
+        //модальное окно "Вы действительно хотите удалить запись?"
         $(".box__delete-press").on("click", function () {
             $(".modal").addClass("show");
             $(".box__delete").addClass("box__delete-active");
             $(".box__delete").addClass("box__delete-active");
-            $("body").addClass('no-scroll'); 
+            $("body").addClass('no-scroll');
         }),
         $(".box__delete-close").on("click", function () {
             $(".modal").removeClass("show");
             $(".box__delete").removeClass("box__delete-active");
-            $("body").removeClass('no-scroll'); 
+            $("body").removeClass('no-scroll');
         }),
         $(".button__delete-cloce").on("click", function () {
             $(".modal").removeClass("show");
             $(".box__delete").removeClass("box__delete-active");
-            $("body").removeClass('no-scroll'); 
+            $("body").removeClass('no-scroll');
         }),
         $(".modal").on("click", function () {
             $(".modal").removeClass("show");
             $(".box__delete").removeClass("box__delete-active");
-            $("body").removeClass('no-scroll'); 
+            $("body").removeClass('no-scroll');
         }),
 
         $(".button__delete-edit").on("click", function () {
             $(".form__edit").addClass("form__edit-active");
         }),
+
+        //Модальное окно "Добавить событие"
+        $(".btn-big").on("click", function () {
+            $(".modal").addClass("show");
+            $(".modal__information").addClass("modal__information-active");
+            $("body").addClass('no-scroll');
+        }),
+        $(".btn-elementsAdd").on("click", function () {
+            event.preventDefault();
+            $(".modal").addClass("show");
+            $(".modal__information").addClass("modal__information-active");
+            $("body").addClass('no-scroll');
+        }),
+        $(".modal__information-close").on("click", function () {
+            event.preventDefault();
+            $(".modal__information").removeClass("modal__information-active");
+            $("body").removeClass('no-scroll');
+            $(".modal").removeClass("show");
+        }),
+        $(".modal__information-BTNclose").on("click", function () {
+            event.preventDefault();
+            $(".modal__information").removeClass("modal__information-active");
+            $("body").removeClass('no-scroll');
+            $(".modal").removeClass("show");
+        }),
+        $(".modal").on("click", function () {
+            $(".modal__information").removeClass("modal__information-active");
+            $("body").removeClass('no-scroll');
+            $(".modal").removeClass("show");
+        }),
+        
         // $(".modal").on("click", function () {
         //     $(this).toggleClass("active");
         //     $(".modal").toggleClass("show");
@@ -39,6 +82,7 @@ $(function () {
         //     $(".menu").toggleClass("menu-transform");
         // }),
 
+        //Изменение размера цветных кружочков 
         $(".color1").on("click", function () {
             $(".color1").toggleClass("color1-active");
             $(".color2").removeClass("color2-active");
@@ -55,6 +99,7 @@ $(function () {
             $(".color2").removeClass("color2-active");
         }),
 
+        // Оранжевая рамка при выборе дизайна 
         $(".stylePage__image-1 ").on("click", function () {
             $(".stylePage__img-box-1 ").toggleClass("stylePage__border-active");
             $(".stylePage__img-box-2").removeClass("stylePage__border-active");
@@ -71,6 +116,7 @@ $(function () {
             $(".stylePage__img-box-1").removeClass("stylePage__border-active");
         }),
 
+        // Оранжевая рамка при выборе кол-ва страниц 
         $(".stylePage__btn-1 ").on("click", function () {
             $(".stylePage__btn-1 ").toggleClass("stylePage__border-active");
             $(".stylePage__btn-2").removeClass("stylePage__border-active");
@@ -80,18 +126,52 @@ $(function () {
             $(".stylePage__btn-1").removeClass("stylePage__border-active");
         }),
 
-        $('#privacy-switch').click(function () {
-            if ($(this).is(':checked')) {
-                $(".privacy__form").addClass("privacy__form-active");
-            } else {
-                $(".privacy__form").removeClass("privacy__form-active");
-            }
-        });
+        //Вкладки в модульном окне "Добавить событие"
+        $('input[name="tab"]').click(function () {
+            var target = $('#block-' + $(this).val());
+            $('.block-text').not(target).hide(0);
+            target.fadeIn(100);
+        }),
+        //Радио-кнопки в модульном окне "Выбрать ответ"
+        $('input[name="ack"]').click(function () {
+            var target = $('#block__ask-' + $(this).val());
+            $('.block-form').not(target).hide(0);
+            target.fadeIn(100);
+        }),
+    
+        //Клонирование input
+        $(".info__ack1-add").on('click', function () {
+            event.preventDefault();
+            var ele = $(this).closest('.copy-input').clone(true);
+            $(this).closest('.copy-input').after(ele);
+        }),
+        
+            
+    // Чекбокс "Сделать ваш сайт приватным"
+    $('#privacy-switch').click(function () {
+        if ($(this).is(':checked')) {
+            $(".privacy__form").addClass("privacy__form-active");
+        } else {
+            $(".privacy__form").removeClass("privacy__form-active");
+        }
+    });
+    
+
+    //всплываюзие черные подсказки
     $(document).ready(function () {
         $(".question-icon").hover(function () { // задаем функцию при наведении курсора на элемент и при его отведении	
-            $(".box-map ").toggleClass("box-map--active") // добавляем, или убираем класс у текущего элемента div
+            $(".box__map ").toggleClass("box__map--active") // добавляем, или убираем класс у текущего элемента div
         });
     });
-
+    $(document).ready(function () {
+        $(".question-icon1").hover(function () { // задаем функцию при наведении курсора на элемент и при его отведении	
+            $(".box__info1 ").toggleClass("box__info1--active") // добавляем, или убираем класс у текущего элемента div
+        });
+    });
+    $(document).ready(function () {
+        $(".question-icon2").hover(function () { // задаем функцию при наведении курсора на элемент и при его отведении	
+            $(".box__info2 ").toggleClass("box__info2--active") // добавляем, или убираем класс у текущего элемента div
+        });
+    });
 
 })
